6d5f0e05c66eccf228bdfb62a1f779618961478c
Lorenzo <llwmuchiri@gmail.com>
2025-01-25 17:37:00 +0300

    Made changes to the arrow animation on the list items of the accordion

--- Accordion/Script.js
+++ Accordion/Script.js
@@ -2,19 +2,31 @@ const btn = document.querySelectorAll("button");
 
 btn.forEach((button) => {
   button.onclick = () => {
+    button.classList.add("rotate");
     let parentElement = button.parentElement.parentElement,
       innerDiv = Array.from(parentElement.children).find(
         (element) => element.id == "inner"
       );
-    if (innerDiv.classList.contains("open")) innerDiv.classList.remove("open");
-    else innerDiv.classList.add("open");
+    if (innerDiv.classList.contains("open")) {
+      innerDiv.classList.remove("open");
+      button.classList.remove("rotate");
+    } else {
+      innerDiv.classList.add("open");
+      button.classList.add("rotate");
+    }
 
     //Check the others are closed when one is open
     [...document.querySelectorAll("#inner")]
       .filter((element) => element != innerDiv)
       .forEach((element) => {
-        if (element.classList.contains("open"))
+        let childBtn = Array.from(element.parentElement.children).find(
+          (element) => element instanceof HTMLSpanElement
+        ).children;
+
+        if (element.classList.contains("open")) {
           element.classList.remove("open");
+          childBtn[0].classList.remove("rotate");
+        }
       });
   };
 });
