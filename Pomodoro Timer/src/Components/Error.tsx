1f39d80e69cda5db864faad09486fba980aed8f4
Lorenzo <llwmuchiri@gmail.com>
2025-02-12 11:25:15 +0300

    Deleted a few files

--- Pomodoro Timer/src/Components/Error.tsx
+++
@@ -1,9 +0,0 @@
-const Error = (): React.ReactNode => {
-  return (
-    <div>
-      <h1>Error element</h1>
-    </div>
-  );
-};
-
-export default Error;
--- Pomodoro Timer/src/Components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loader = (): React.ReactNode => {
-  return (
-    <div>
-      <h1>Loader element</h1>
-    </div>
-  );
-};
-
-export default Loader;
